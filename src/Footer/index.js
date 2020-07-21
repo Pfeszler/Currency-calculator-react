8b1dacb98ee9336c15a76eb57b766adeab6a2aac
Pfeszler <pfeszler@gmail.com>
2020-07-21 11:58:51 +0200

    added footer

---
+++ src/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+
+
+const Footer = () => 
+<footer className="footer">
+<p className="footer__text">
+  Nie jesteśmy prawdziwym kantorem. ale i tak możesz przesłać nam pieniądze. Gwarantujemy. że ich nie
+  odzyskasz!
+</p>
+</footer>
+
+export default Footer
