e80001e47287d4fbf25a2992bfef60b84b7b9eb3
Pfeszler <pfeszler@gmail.com>
2020-07-21 13:48:45 +0200

    Updates

---
+++ precache-manifest.682438ad3a2fe30c96565d6a9de7c5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11e118451817ba0d4348793b813f132",
+    "url": "/Currency-calculator-react/index.html"
+  },
+  {
+    "revision": "e4bd1c3843a81294e659",
+    "url": "/Currency-calculator-react/static/css/main.c161f0c0.chunk.css"
+  },
+  {
+    "revision": "a1a80dfed30d99b64fda",
+    "url": "/Currency-calculator-react/static/js/2.fd8f644b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-calculator-react/static/js/2.fd8f644b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bd1c3843a81294e659",
+    "url": "/Currency-calculator-react/static/js/main.7d21f024.chunk.js"
+  },
+  {
+    "revision": "ee36364f033663193d39",
+    "url": "/Currency-calculator-react/static/js/runtime-main.4515b25a.js"
+  },
+  {
+    "revision": "dcdf811768e1e387af3232034b082fec",
+    "url": "/Currency-calculator-react/static/media/money_banknotes_currency_forex_us_dollars_euro_baht_peso-551241.dcdf8117.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Currency-calculator-react/precache-manifest.682438ad3a2fe30c96565d6a9de7c5bd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Currency-calculator-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
